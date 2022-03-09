2af6c518471c59526d131ac2a57d27e1a55df3da
higor <higorjardini07@gmail.com>
2022-03-09 18:21:40 -0300

    Release(Alura): Java 4

---
+++ Java/Fundamentals_of_Java/Fundamental_3/syntax-variables-and-flux/src/TestValues.java
@@ -0,0 +1,18 @@
+
+public class TestValues {
+	
+	// Generated this function main with shortcut: CTRL + Spacebar and selected "main"
+
+	public static void main(String[] args) {
+		int first  = 5;
+		int second = 7;
+		
+		second = first;
+		first  = 10;
+		
+		// Comment: attribution value and not sync values from variables
+		
+		System.out.println(second);
+	}
+
+}
